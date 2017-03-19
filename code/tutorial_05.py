793513ea4c2e29ff4a0066e2d80cf581b69ed0df
Jeremy England <simplycoded.help@gmail.com>
2017-03-19 10:29:13 -0500

    Create tutorial_05.py

---
+++ code/tutorial_05.py
@@ -0,0 +1,21 @@
+### can be any letter in the alphabet - lower or uppercase
+a = "Hi"
+mYNUMberIs = 23
+camelCaseExample = 45
+PascalCaseExample = 923
+
+### can have numbers in them - except at the very beginning
+number2is = 2
+a138913fjalf334 = "Horrible example, but works"
+#2wrong = 123
+
+### cannot contain any spaces
+#this is wrong = 123
+
+### can have underscores anywhere in them
+_another__example_ = 654
+
+### convention: All lowercase, every word is separated by an underscore.
+flavor = "mint"
+girls_number = 123456789
+first_last_name = "Jeremy England"
