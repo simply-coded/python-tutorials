c4e4ad53cf89a2cb79695774a4f4f72f64ac07a0
Jeremy England <simplycoded.help@gmail.com>
2017-03-19 18:08:16 -0500

    Create tutorial_07.py

---
+++ code/tutorial_07.py
@@ -0,0 +1,30 @@
+a = "hello"
+b = 'there'
+c = 'hello' + 'there'
+d = 'hello' 'there'
+e = 'hello ' + b
+f = a + b
+g = a + ' ' + b
+h = "I'm using an apostrophe"
+i = 'I will "use a quote" in my string.'
+j = '"Let\'s use both" - she said'
+k = "\"Let's use both\" - he said"
+l = '''"Let's use both" - they said'''
+m = """"Let's use both" - it said"""
+n = ('I span across '
+     'multiple lines '
+     'however, I remain as one line. '
+     'Also, ' +
+     g +
+     ' nice to meet you.')
+o = '''I span across 
+multiple lines too,
+however, I become multiple lines.'''
+p = """
+I do what that guy 
+does as well. 
+  Also note how I keep my 
+        indentation
+"""
+q = str(32)
+r = str(15.23)
