15e3bf610b9caa4c63b419a3b467e046cab2a62c
Jeremy England <simplycoded.help@gmail.com>
2017-03-18 20:33:58 -0500

    Create tutorial_04.py

---
+++ code/tutorial_04.py
@@ -0,0 +1,26 @@
+"""I'm a docstring
+
+  I describe what this file is
+"""
+
+# I'm a comment.
+print("Do you want some icecream")
+
+# Comments can be used to ignore code
+#print("I don't like icecream")
+
+print( __doc__ ) # comments can be at the end of statements
+
+'''
+I
+am 
+a 
+multi-line
+comment
+'''
+
+"""
+So 
+am 
+I
+"""
