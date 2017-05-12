5a422b532cdb467ec441877a91de8a7ebb9a5426
Jeremy England <simplycoded.help@gmail.com>
2017-05-12 11:21:11 -0500

    Create tutorial_17.py

---
+++ code/tutorial_17.py
@@ -0,0 +1,38 @@
+#assign a value to a variable
+val = 5
+
+#add 2 and save result
+val += 2
+
+#subtract 2 and save result
+val -= 2
+
+#multiply by 2 and save result
+val *= 2
+
+#raise to the 2nd power and save result
+val **= 2
+
+#divide by 2 and save result
+val /= 2
+
+#floor divide by 2 and save result
+val //= 2
+
+#modulo by 2 and save result
+val %= 2
+
+#shift bits to the right by 2 and save result
+val >>= 2
+
+#shift bits to the left by 2 and save result
+val <<= 2
+
+#perform a bitwise and by 2 and save result
+val &= 2
+
+#perform a bitwise or by 2 and save result
+val |= 2
+
+#perform a bitwise exclusive or by 2 and save result
+val ^= 2
