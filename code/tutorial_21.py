bf968aad32a0815d6de95f7414bebf0c54b98ee8
Jeremy England <simplycoded.help@gmail.com>
2017-05-29 22:03:48 -0500

    Create tutorial_21.py

---
+++ code/tutorial_21.py
@@ -0,0 +1,29 @@
+# basic
+if True:
+    print('i will run')
+
+if False:
+    print('i will not run')    
+
+# video example
+inventory = {'chairs': 20, 'tables': 5, 'drinks': 103}
+actual = 130
+counted = sum(inventory.values())
+is_invalid = False
+
+if counted == actual:
+    print('all items here')
+elif counted > actual:
+    print('extra items')
+elif counted < actual:
+    if counted < 0:
+        print('negative input. please check again')
+    else:
+        print('missing items')
+else:
+    print('invalid input')
+    is_invalid = True
+
+
+if not is_invalid:
+    print('inventory check complete')
