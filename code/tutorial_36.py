4223a143ad37ac24fb5b2d897df626e5a1ea4ffb
Jeremy England <simplycoded.help@gmail.com>
2017-06-28 20:06:22 -0500

    Create tutorial_36.py

---
+++ code/tutorial_36.py
@@ -0,0 +1,36 @@
+try:   
+
+    def call(location_reply):
+        print('Pete!!!!!!! . . .', location_reply)    
+        
+    def house():
+        
+        def bedroom():
+
+            def bathroom():
+                pass
+                # BATHROOM
+                # pete = 'I am in the bathroom.' 
+                # call(pete)                
+               
+            def closet():
+                pass  
+                # CLOSET                           
+                # pete = 'I am in the closet.'                
+                # call(pete)                   
+            
+            # BEDROOM               
+            # pete = 'I am in the bedroom.'
+            # call(pete)                    
+        
+        # HOUSE                
+        # pete = 'I am in the house.'
+        # call(pete)   
+        
+    # OUTSIDE/GLOBAL
+    # pete = 'I am outside.'       
+    # call(pete)       
+    
+except NameError:        
+    pete = '*no response*'
+    call(pete)
